fd73d70a37d8fc6e8b3f477ebae56344d2620238
sami <swamy.kantri@gmail.com>
2022-07-28 09:41:28 +0530

    added new class for git

---
+++ src/test/java/SWMavenProj/ClassGit.java
@@ -0,0 +1,11 @@
+package SWMavenProj;
+
+public class ClassGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("GIT1");
+System.out.println("GIT2");
+	}
+
+}
